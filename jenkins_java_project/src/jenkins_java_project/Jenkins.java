7e99e3cb5d2568f450ee3c31afe5f8a0ad5d6c55
amaalouf38 <maalouf.assad@gmail.com>
2023-08-06 16:42:27 -0500

    commit message

--- jenkins_java_project/src/jenkins_java_project/Jenkins.java
+++ jenkins_java_project/src/jenkins_java_project/Jenkins.java
@@ -4,15 +4,6 @@ public class Jenkins {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//this is a new commit for the repository
-//new changes to the file		
-		
-		//I also have changes
-		//I also have changes
-		
-		//new changes to the file
-		
-		//this is a new commit for the repository
 
 	}
 

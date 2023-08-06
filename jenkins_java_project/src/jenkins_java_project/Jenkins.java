63ce0955a0d961ea25f20eb59aff8b3db02bbe11
amaalouf38 <maalouf.assad@gmail.com>
2023-08-06 15:27:06 -0500

    first commit

---
+++ jenkins_java_project/src/jenkins_java_project/Jenkins.java
@@ -0,0 +1,10 @@
+package jenkins_java_project;
+
+public class Jenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//this is a new commit for the repository
+	}
+
+}
